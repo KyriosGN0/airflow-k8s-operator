f64971ef4545496db45dff6fcc91b310688a04f3
drfaust92 <ilia.lazebnik@gmail.com>
2025-11-21 11:10:14 -0500

    initial commit

--- config.py
+++ config.py
@@ -1,12 +1,16 @@
 import os
 import airflow_client.client as client
 
-AIRFLOW_HOST = os.getenv("AIRFLOW_HOST")  # get from env var
+AIRFLOW_HOST = os.getenv("AIRFLOW_HOST")
 if not AIRFLOW_HOST:
 	raise RuntimeError("Environment variable AIRFLOW_HOST must be set")
 
-# obtain an access token (implement generate_access_token elsewhere)
-access_token = generate_access_token("admin", "admin", AIRFLOW_HOST)  # generate from gcp for composer instead
+AIRFLOW_USERNAME = os.getenv("AIRFLOW_USERNAME", "admin") # temp until i abstract the auth logic for other vendors as well
+AIRFLOW_PASSWORD = os.getenv("AIRFLOW_PASSWORD", "admin") # temp until i abstract the auth logic for other vendors as well
 
-configuration = client.Configuration(host=AIRFLOW_HOST, access_token=access_token)
+if AIRFLOW_USERNAME and AIRFLOW_PASSWORD:
+	configuration = client.Configuration(host=AIRFLOW_HOST, username=AIRFLOW_USERNAME, password=AIRFLOW_PASSWORD)
+else:
+	raise RuntimeError("Environment variable AIRFLOW_USERNAME and AIRFLOW_PASSWORD must be set")  # temp until i abstract the auth logic for other vendors as well
+
 api_client = client.ApiClient(configuration=configuration)
