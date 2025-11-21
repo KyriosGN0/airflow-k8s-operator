c1c8ccbd0a0bbf1f2772b0c1cf27899936b1eeaf
drfaust92 <ilia.lazebnik@gmail.com>
2025-11-21 12:03:02 -0500

    initial commit

--- config.py
+++ config.py
@@ -5,12 +5,15 @@ AIRFLOW_HOST = os.getenv("AIRFLOW_HOST")
 if not AIRFLOW_HOST:
 	raise RuntimeError("Environment variable AIRFLOW_HOST must be set")
 
-AIRFLOW_USERNAME = os.getenv("AIRFLOW_USERNAME", "admin") # temp until i abstract the auth logic for other vendors as well
-AIRFLOW_PASSWORD = os.getenv("AIRFLOW_PASSWORD", "admin") # temp until i abstract the auth logic for other vendors as well
+AIRFLOW_USERNAME = os.getenv("AIRFLOW_USERNAME")
+AIRFLOW_PASSWORD = os.getenv("AIRFLOW_PASSWORD")
+AIRFLOW_ACCESS_TOKEN = os.getenv("AIRFLOW_ACCESS_TOKEN")
 
 if AIRFLOW_USERNAME and AIRFLOW_PASSWORD:
 	configuration = client.Configuration(host=AIRFLOW_HOST, username=AIRFLOW_USERNAME, password=AIRFLOW_PASSWORD)
+elif AIRFLOW_ACCESS_TOKEN:
+	configuration = client.Configuration(host=AIRFLOW_HOST, access_token=AIRFLOW_ACCESS_TOKEN)
 else:
-	raise RuntimeError("Environment variable AIRFLOW_USERNAME and AIRFLOW_PASSWORD must be set")  # temp until i abstract the auth logic for other vendors as well
+	raise RuntimeError("Environment variable (AIRFLOW_USERNAME and AIRFLOW_PASSWORD) or AIRFLOW_ACCESS_TOKEN must be set")
 
 api_client = client.ApiClient(configuration=configuration)
--- variables.py
+++ variables.py
@@ -1,9 +1,9 @@
 import kopf
-import airflow_client.client as client
+from airflow_client.client.api.variable_api import VariableApi
 from airflow_client.client.model.variable import Variable
 from config import api_client
 
-variables_api = client.apis.VariableApi(api_client=api_client)
+variables_api = VariableApi(api_client=api_client)
 @kopf.on.create('airflow.drfaust92', 'v1beta1', 'variables')
 def create_variable(meta, spec, namespace, logger, body, **kwargs):
     var_name = spec.get('name')
@@ -36,3 +36,25 @@ def delete_variable(meta, spec, namespace, logger, body, **kwargs):
     except Exception as e:
         logger.error(f"Failed to delete Airflow Variable {var_name}: {e}")
         return {'error': f'Failed to delete variable {var_name}: {e}'}
+    
+@kopf.on.update('airflow.drfaust92', 'v1beta1', 'variables')
+def update_variable(meta, spec, namespace, logger, body, **kwargs):
+    var_name = spec.get('name')
+    var_value = spec.get('value')
+    var_description = spec.get('description', '')
+
+    logger.info(f"Updating Airflow Variable: {var_name} with value: [MASKED] and description: {var_description}")
+    try:
+        variable = Variable(
+            key=var_name,
+            value=var_value,
+            description=var_description
+        )
+        variables_api.patch_variable(
+            variable_key=var_name,
+            variable=variable
+            )
+        return {'message': f'Variable {var_name} updated successfully.'}
+    except Exception as e:
+        logger.error(f"Failed to update Airflow Variable {var_name}: {e}")
+        return {'error': f'Failed to update variable {var_name}: {e}'} 
